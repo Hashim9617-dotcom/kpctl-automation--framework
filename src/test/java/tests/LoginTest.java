2f57955f49d8ddd2823699aa5cfeb930033e999c
hashim khan <hashim.khan@extranet.ae>
2026-04-16 13:20:15 +0530

    update

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,34 +1,28 @@
 package tests;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.UiPathRunner;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoginTest {
 
-    // Logger declaration (CLASS LEVEL)
     private static final Logger log = LogManager.getLogger(LoginTest.class);
 
     @Test
     public void loginViaUiPath() {
 
-        log.info("Starting UiPath bot");
+        log.info("Starting test execution");
 
-        // Step 1: Run UiPath bot
-        UiPathRunner.runBot();
 
         try {
-            Thread.sleep(10000); // wait for bot
+            Thread.sleep(5000); // optional wait
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        log.info("Bot execution completed");
+        log.info("Test execution completed");
 
-        // Dummy validation (for now pass karne ke liye)
         Assert.assertTrue(true);
     }
+   
 }
